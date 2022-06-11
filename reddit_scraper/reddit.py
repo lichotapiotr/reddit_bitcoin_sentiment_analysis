c9f47babf613c9a2d5a6fc0346a7a3b5dc5b56ed
piotrlichota <lichotapiotrek@gmail.com>
2022-06-11 14:11:19 +0200

    Scraper improved #es searching comments

--- reddit_scraper/reddit.py
+++ reddit_scraper/reddit.py
@@ -10,10 +10,15 @@ end_epoch = int(dt.datetime(2022, 5, 31).timestamp())
 
 
 def scraper(start_epoch, end_epoch):
-    api_request_generator = api.search_submissions(subreddit='Bitcoin', limit=1000, after=start_epoch, before=end_epoch)
-    df_reddit = pd.DataFrame([submission.d_ for submission in api_request_generator])
-    df_reddit = df_reddit[['author', 'author_flair_css_class', 'created_utc', 'id', 'num_comments', 'score', 'selftext', 'title', 'upvote_ratio']]
+    #api_request_generator = api.search_submissions(subreddit='Bitcoin', limit=1000, after=start_epoch, before=end_epoch)
+    #df_reddit = pd.DataFrame([submission.d_ for submission in api_request_generator])
+    api_request_generator = api.search_comments(subreddit='Bitcoin', limit=100, after=start_epoch, before=end_epoch,
+                                                filter=['author', 'created_utc', 'title', 'body', 'score', 'id'])
+    df_reddit = pd.DataFrame([comment.d_ for comment in api_request_generator])
+    #df_reddit = df_reddit[['author', 'author_flair_css_class', 'created_utc', 'id', 'num_comments', 'score', 'selftext', 'title', 'upvote_ratio']]
+    df_reddit['created_utc'] = pd.to_datetime(df_reddit['created_utc'], utc=True, unit='s')
     return df_reddit
+
 
 
 if __name__ == '__main__':
