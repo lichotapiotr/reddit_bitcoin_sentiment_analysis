f025067f067176b11772ae105dc016c285bcabcf
VerSuchy <m.sucharzewski1@gmail.com>
2022-06-27 20:26:18 +0200

    nowy pearson i spearman oraz przesuniety wykres o 3 dni

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
 
-from reddit_scraper.reddit import api_scraper, start_epoch, end_epoch
-from sentiment.sentiment import sentiment_analyzis
+# from reddit_scraper.reddit import api_scraper, start_epoch, end_epoch
+# from sentiment.sentiment import sentiment_analyzis
 
 if __name__ == '__main__':
-    reddit_df = api_scraper(start_epoch, end_epoch)
-    analyzed_df = sentiment_analyzis(reddit_df)
+    # reddit_df = api_scraper(start_epoch, end_epoch)
+    # analyzed_df = sentiment_analyzis(reddit_df)
 
-    # analyzed_df = pd.read_csv(r'C:\Users\suchy\Downloads\bitcoin_analyzis.csv')
+    analyzed_df = pd.read_csv(r'C:\Users\suchy\Downloads\bitcoin_analyzis.csv')
     bitcoin_df = pd.read_csv(r'C:\Users\suchy\Downloads\Bitcoin.csv')
 
     analyzed_df = analyzed_df.drop([0, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163])
@@ -37,6 +38,25 @@ if __name__ == '__main__':
     sentiment_array = correlation_df['sentiment_score'].to_numpy()
     opening_array = correlation_df['Otwarcie'].to_numpy()
     pearson = np.corrcoef(sentiment_array, opening_array)
+
     print(pearson)
     print(bitcoin_df['Otwarcie'].describe())
+
+    bitcoin_df = bitcoin_df.set_index('Data')
+    bitcoin_df = bitcoin_df.shift(periods=3)
+    bitcoin_df = bitcoin_df.reset_index()
+
+    ax = bitcoin_df.plot(x='Data', y='Otwarcie', color='r', kind='line')
+    analyzed_df.plot(ax=ax, x='time', y='sentiment_score', kind='line')
+    plt.show()
+
+    correlation_df = analyzed_df.merge(bitcoin_df[['time', 'Otwarcie']], on='time')
+    sentiment_array = correlation_df['sentiment_score'].to_numpy()
+    opening_array = correlation_df['Otwarcie'].to_numpy()
+    pearson = np.corrcoef(sentiment_array, opening_array)
+
+    print(pearson)
+
+    print(stats.spearmanr(sentiment_array, opening_array))
+
 
